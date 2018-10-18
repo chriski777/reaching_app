7bd78256d5360d901281f0fe4ed3c0ffc155e4d9
Chris <chriski777@berkeley.edu>
2018-10-18 15:11:49 -0700

    Added differential processing of lick recordings

--- neoPixelRing/NeoPixels.ino
+++ neoPixelRing/NeoPixels.ino
@@ -1,6 +1,10 @@
 #include <Adafruit_NeoPixel.h>
 #define PIN 6 // the Pin that will be our source of input
 
+int lickPin = 0;
+int foilPin = 1;
+int val = 0;
+int val2 =0;
 int totalPixels = 24;
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(totalPixels, PIN, NEO_RGBW + NEO_KHZ800);
 
@@ -10,6 +14,7 @@ uint32_t pureBrightWhite = strip.Color(0, 0, 0, 255);
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
   strip.begin();
   strip.show(); // Initialize all pixels to 'off'
 }
@@ -18,10 +23,13 @@ void loop() {
   // put your main code here, to run repeatedly:
   setAllPixels(24,pureWhite);
   strip.show();
-  delay(5000);
-  setAllPixels(24,pureOff);
-  strip.show();
-  delay(5000);
+  val = analogRead(lickPin);
+  val2 = analogRead(foilPin);
+  Serial.println(val- val2);
+//  delay(5000);
+//  setAllPixels(24,pureOff);
+//  strip.show();
+//  delay(5000);
 }
 
 void setAllPixels(int numPixel, uint32_t color) {
