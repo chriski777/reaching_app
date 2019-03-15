2c4f1b9d8998214bf55299d571cbfbd2088c090f
Chris <chriski777@berkeley.edu>
2019-03-15 16:01:50 -0700

    Added point drawing functionality

--- camera/camera_gui.py
+++ camera/camera_gui.py
@@ -2,41 +2,68 @@ from ximea import xiapi
 from collections import deque
 import PIL.Image, PIL.ImageTk
 import Tkinter as tki 
+import tkMessageBox
 import threading 
 import cv2
 import time
 import cam_func.camera_start_acq as csa
 class CameraApp(tki.Frame):
 	def __init__(self, window, window_title):
+		self.streaming = False
+		self.draw_mode = False
+		self.temp_lines = []
+		self.saved_lines =  []
 		self.window = window
+		self.window.geometry("1280x960")
 		self.window.title(window_title)
-		self.vid_stream = VideoStream()
-		self.canvas = tki.Canvas(window, width = self.vid_stream.img_width, height = self.vid_stream.img_height)
-		self.canvas.pack()
-		self.delay = 15
-		self.update()
+		self.createWidgets()
 		self.window.mainloop()
-		
+	def free_run(self):
+		if not(self.streaming):
+			self.vid_stream = VideoStream()
+			self.canvas = tki.Canvas(self.window, width = self.vid_stream.img_width, height = self.vid_stream.img_height)
+			self.canvas.pack()
+			self.delay = 15
+			self.update()
+			self.streaming = True
+		else: 
+			tkMessageBox.showinfo("Warning", "Streaming is already on.")
+	def draw_cursor(self, event):
+		self.window.config(cursor = "cross")
+	def draw_single(self, event):
+		self.temp_lines.append([event.x,event.y, event.x+1, event.y])
+	def draw_pixels(self):
+		if not(self.streaming):
+			tkMessageBox.showinfo("Warning", "Streaming must be on to draw polygons.")
+		else: 
+			self.draw_mode = True
+			canvas = self.canvas
+	def createWidgets(self):
+		self.free_run_btn = tki.Button()
+		self.free_run_btn["text"] = "Free Run"
+		self.free_run_btn["command"] =  self.free_run
+
+		self.free_run_btn.pack({"side": "left"})
+
+		self.draw_pixels_btn = tki.Button()
+		self.draw_pixels_btn["text"] = "Choose POIs"
+		self.draw_pixels_btn["command"] = self.draw_pixels
+		self.draw_pixels_btn.pack({"side": "left"})
 	def update(self):
 		ret = csa.grab_image(self.vid_stream.num_cameras, self.vid_stream.img_dict)
 		if ret:
 			data_arr = [self.vid_stream.img_dict[i].get_image_data_numpy() for i in range(self.vid_stream.num_cameras)]
 			self.photo = PIL.ImageTk.PhotoImage(image = PIL.Image.fromarray(data_arr[0]))
 			self.canvas.create_image(0,0, image = self.photo, anchor = tki.NW)
+			if self.saved_lines:
+				for sl in self.saved_lines:
+					self.canvas.create_line(sl[0], sl[1], sl[2], sl[3])
+			if self.draw_mode:
+				self.window.bind('<Motion>', self.draw_cursor)
+				self.window.bind('<Button-1>', self.draw_single)
+				for line in self.temp_lines:
+					self.canvas.create_line(line[0], line[1], line[2], line[3], width = 5, fill = 'red')
 		self.window.after(self.delay,self.update)
-
-	def createWidgets(self):
-		self.free_run_btn = tki.Button(self)
-		self.free_run_btn["text"] = "Free Run "
-		self.free_run_btn["command"] =  self.free_run 
-
-		self.free_run_btn.pack({"side": "left"})
-
-		self.draw_polygon = tki.Button(self)
-		self.draw_polygon["text"] = "Hello"
-		self.draw_polygon["command"] = self.free_run
-
-		self.draw_polygon.pack({"side": "left"})
 class VideoStream:
 	def __init__(self):
 		self.acquisition = False
